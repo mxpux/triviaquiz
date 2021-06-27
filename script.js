c1dcc00c9ad9a1041b79a50f2317348d198ab8a8
MPx <hellomariap@gmail.com>
2021-06-26 20:40:25 -0700

    Added JS: Start button timer, start button start game

--- script.js
+++ script.js
@@ -5,6 +5,33 @@
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 
+document.getElementById("startbutton").addEventListener("click", function(){
+    var timeleft = 3;
+
+    var countDown = setInterval(function function1(){
+    document.getElementById("timeCounter").innerHTML = "00:"+timeleft;
+
+    timeleft -= 1;
+    if(timeleft === 0){
+        clearInterval(countDown);
+        document.getElementById("timeCounter").innerHTML = "GAME OVER"
+    }
+    }, 1000);
+
+    console.log(timeCounter);
+});
+
+// When I click on start
+// then question 1 appears
+
+    const startQuiz = document.getElementById('startbutton')
+    startQuiz.addEventListener('click', startGame)
+
+    function startGame (){
+        console.log("started")
+        startQuiz.classList.add("hide")
+
+    }
 
 // WHEN I answer a question
 // THEN I am presented with another question
